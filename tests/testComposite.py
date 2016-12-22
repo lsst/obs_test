2ce83a1e94f448b61592eca2863b5a4a40b7d857
Russell Owen <rowen@uw.edu>
2016-12-22 06:58:57 -0800

    Make code compatible with pybind11

--- tests/testComposite.py
+++ tests/testComposite.py
@@ -28,6 +28,8 @@ import pickle
 import shutil
 import unittest
 
+from lsst.afw.image.testUtils import makeRampImage
+import lsst.afw.geom as afwGeom
 import lsst.afw.image as afwImage
 import lsst.daf.persistence as dafPersist
 import lsst.obs.test
@@ -35,8 +37,21 @@ import lsst.utils.tests
 from lsst.utils import getPackageDir
 
 
+def makeRampDecoratedImage(bbox, start, **metadataDict):
+    """Make a DecoratedImageU that is a ramp
+    """
+    rampArr = makeRampImage(bbox=bbox, start=start, imageClass=afwImage.ImageU).getArray()
+    decoratedImage = afwImage.DecoratedImageU(bbox)
+    imarr = decoratedImage.getImage().getArray()
+    imarr[:] = rampArr
+    md = decoratedImage.getMetadata()
+    for (key, val) in metadataDict.items():
+        md.set(key, val)
+    return decoratedImage
 
-class TestCompositeTestCase(unittest.TestCase):
+
+class TestCompositeTestCase(lsst.utils.tests.TestCase):
+
     """A test case for composite object i/o."""
 
     def setUp(self):
@@ -45,54 +60,58 @@ class TestCompositeTestCase(unittest.TestCase):
         self.output = os.path.join(obsTestDir, 'tests', 'outputs')
         self.compositeOutput = os.path.join(self.output, 'composite')
         self.nonCompositeOutput = os.path.join(self.output, 'noncomposite')
+        self.dataId = {'visit': 1, 'filter': 'g'}
 
     def tearDown(self):
         if os.path.exists(self.output):
             shutil.rmtree(self.output)
 
-    def test(self):
+    def testGet(self):
         """Verify that a composite can be loaded and that its components are the same as when the type1
         components are loaded individually (verifies correct lookup in this case).
-        Also verify that when the individual components are put and when the composite is put (which
+        """
+        butler = dafPersist.Butler(
+            inputs=dafPersist.RepositoryArgs(root=self.input, mapper='lsst.obs.test.testMapper.TestMapper'),
+        )
+        rawAndFlat = butler.get('rawAndFlat', dataId=self.dataId)
+        raw = butler.get('raw', dataId=self.dataId)
+        flat = butler.get('flat', dataId=self.dataId)
+
+        # Exposures (which is how these are retrieved) cannot be directly compared,
+        # including associated objects and metadata, so pickle them and cmpare the pickles
+        self.assertEqual(pickle.dumps(raw), pickle.dumps(rawAndFlat.raw))
+        self.assertEqual(pickle.dumps(flat), pickle.dumps(rawAndFlat.flat))
+
+    def testPut(self):
+        """Verify that when the individual components are put and when the composite is put (which
         disassembles into individual components) that the objects that are written are the same.
+
+        Note that raw and calibration frames do not round trip (they are saved as DecoratedImageU
+        and read in as ExposureU), so create the raw and flat manually.
         """
         outputs = (dafPersist.RepositoryArgs(root=self.compositeOutput, tags='composite'),
                    dafPersist.RepositoryArgs(root=self.nonCompositeOutput, tags='noncomposite'))
-        butler = dafPersist.Butler(inputs=dafPersist.RepositoryArgs(root=self.input,
-            mapper='lsst.obs.test.testMapper.TestMapper'), outputs=outputs)
-        rawAndFlat = butler.get('rawAndFlat', dataId={'visit':1, 'filter':'g'})
-        raw = butler.get('raw', dataId={'visit':1, 'filter':'g'})
-        flat = butler.get('flat', dataId={'filter':'g'})
+        butler = dafPersist.Butler(
+            inputs=dafPersist.RepositoryArgs(root=self.input, mapper='lsst.obs.test.testMapper.TestMapper'),
+            outputs=outputs)
+        bbox = afwGeom.Box2I(afwGeom.Point2I(0, 0), afwGeom.Point2I(10, 10))
+        raw = makeRampDecoratedImage(bbox=bbox, start=100, raw1=5, raw2="hello")
+        flat = makeRampDecoratedImage(bbox=bbox, start=-55, flat1="me", flat2=47)
 
-        # verify that the flat & raw loaded by the composite object are correct by loading the non-composite
-        # flat & raw, serializing (via pickle) and using filecmp to check that they are the same.
-        flatPath = os.path.join(self.output, 'pickleFlat')
-        compositeFlatPath = os.path.join(self.output, 'pickleCompositeFlat')
-        with open(flatPath, 'wb') as f:
-            pickle.dump(flat, f)
-        with open(compositeFlatPath, 'wb') as f:
-            pickle.dump(rawAndFlat.flat, f)
-        self.assertTrue(filecmp.cmp(flatPath, compositeFlatPath))
+        # a hack-ish way of creating a rawAndFlat object; read in the data, then replace it with our own
+        rawAndFlat = butler.get('rawAndFlat', dataId=self.dataId)
+        rawAndFlat.raw = raw
+        rawAndFlat.flat = flat
 
-        rawPath = os.path.join(self.output, 'pickleRaw')
-        compositeRawPath = os.path.join(self.output, 'pickleCompositeRaw')
-        with open(rawPath, 'wb') as f:
-            pickle.dump(raw, f)
-        with open(compositeRawPath, 'wb') as f:
-            pickle.dump(rawAndFlat.raw, f)
-        self.assertTrue(filecmp.cmp(rawPath, compositeRawPath))
-
-        butler.put(rawAndFlat, 'rawAndFlat', dataId=dafPersist.DataId({'visit':1, 'filter':'g'}, tags='composite'))
-        butler.put(raw, 'raw', dataId=dafPersist.DataId({'visit':1, 'filter':'g'}, tags='noncomposite'))
-        butler.put(raw, 'flat', dataId=dafPersist.DataId({'filter':'g'}, tags='noncomposite'))
+        butler.put(rawAndFlat, 'rawAndFlat', dataId=self.dataId, tags='composite')
+        butler.put(raw, 'raw', dataId=self.dataId, tags='noncomposite')
+        butler.put(raw, 'flat', dataId=self.dataId, tags='noncomposite')
 
         self.assertTrue(filecmp.cmp(os.path.join(self.compositeOutput, 'raw', 'raw_v1_fg.fits.gz'),
                                     os.path.join(self.nonCompositeOutput, 'raw', 'raw_v1_fg.fits.gz')))
 
         self.assertTrue(filecmp.cmp(os.path.join(self.compositeOutput, 'flat', 'flat_fg.fits.gz'),
                                     os.path.join(self.nonCompositeOutput, 'flat', 'flat_fg.fits.gz')))
-
-
 
 
 class MemoryTester(lsst.utils.tests.MemoryTestCase):
