dd63b84cd4243ee597a90ea05e5a67347449e57d
Simon Krughoff <krughoff@lsst.org>
2019-06-11 21:22:16 +0000

    Update config for tickets/DM-19857
    This means the tickets/DM-18739 ticket will need to be merged with the
    tickets/DM-19857 branch of ap_verify

--- config/datasetIngest.py
+++ config/datasetIngest.py
@@ -4,5 +4,6 @@ import os.path
 from lsst.utils import getPackageDir
 obsConfigDir = os.path.join(getPackageDir('obs_test'), 'config')
 
+config.textDefectPath = os.path.join(getPackageDir('obs_test_data'), 'test', 'defects')
 config.dataIngester.load(os.path.join(obsConfigDir, 'ingest.py'))
 config.calibIngester.load(os.path.join(obsConfigDir, 'ingestCalibs.py'))
